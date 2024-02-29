90354fc5f233450de2f38121b3b0ad00d2b2e4ec
watcher-eth1 <watchereth10@gmail.com>
2024-02-29 13:22:54 -0800

    small fixes

--- packages/contracts/src/systems/NewTopicSystem.sol
+++ packages/contracts/src/systems/NewTopicSystem.sol
@@ -5,22 +5,12 @@ import { System } from "@latticexyz/world/src/System.sol";
 import { Topics, TopicsData } from "../codegen/tables/Topics.sol";
 import { Bets, BetsData } from "../codegen/tables/Bets.sol";
 
-import { Counters } from "../codegen/tables/Counters.sol";
 
 contract NewTopicSystem is System {
-  function incrementCounter(uint32 counter) internal returns (uint32) {
-    // Retrieve the current counter value
-    uint32 currentCounter = Counters.get(counter);
-    uint32 newCounter = currentCounter + 1;
 
-    // Update the counter value in the table
-    Counters.set(counter, newCounter);
-
-    return newCounter;
-  }
 
   function addTopic(string memory name, string memory genre, string memory description) public {
-    uint32 newTopicId = incrementCounter(1);
+    uint32 newTopicId;
     Topics.set(newTopicId, TopicsData({ name: name, genre: genre, description: description }));
   }
   function getTopic(uint32 id) public view returns (TopicsData memory) {
